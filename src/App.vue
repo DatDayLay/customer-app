0f76a9d1e6db5aa60e3e627eda060301d720f90c
DatDayLay <94358210+DatDayLay@users.noreply.github.com>
2025-01-19 10:53:54 +0100

    quick minor bug fix

--- src/App.vue
+++ src/App.vue
@@ -32,38 +32,40 @@ function deleteCustomer(customer) {
 </script>
 
 <template>
-  <Navbar />
-  <div class="bg-slate-50 w-full flex flex-row justify-between py-4">
-    <h1 class="sm:text-2xl text-sky-950 py-3 mr-auto sm:ml-28 ml-6">
-      <i
-        class="pi pi-users text-sky-950 sm:text-2xl text-xl m-auto sm:px-0.5"
-      ></i>
-      Customers
-    </h1>
-    <button
-      class="bg-pink-300 text-white text-sm px-2 py-2 rounded-lg hover:bg-pink-400 w-fit my-auto ml-auto sm:mr-20 mr-4"
-      @click="openAddCustomer"
-    >
-      <i class="pi pi-plus text-white text-sm m-auto px-0.5"></i>
-      Add Customer
-    </button>
-  </div>
-  <CustomerModal
-    v-if="isModalOpen"
-    :customer="selectedCustomer"
-    @close="closeCustomerModal"
-  />
-  <div class="mt-10">
-    <TanstackTable @edit="openEditCustomer" @delete="deleteCustomer" />
-  </div>
-  <footer class="w-full flex justify-center fixed bottom-10">
-    <p class="p-4">
-      Design inspo:
-      <a
-        href="https://dribbble.com/shots/2696581-StrefaKoloru-Customers/attachments/544530?mode=media"
-        class="text-sky-950 underline"
-        >Link</a
+  <div class="h-screen w-full flex flex-col">
+    <Navbar />
+    <div class="bg-slate-50 w-full flex flex-row justify-between py-4">
+      <h1 class="sm:text-2xl text-sky-950 py-3 mr-auto sm:ml-28 ml-6">
+        <i
+          class="pi pi-users text-sky-950 sm:text-2xl text-xl m-auto sm:px-0.5"
+        ></i>
+        Customers
+      </h1>
+      <button
+        class="bg-pink-300 text-white text-sm px-2 py-2 rounded-lg hover:bg-pink-400 w-fit my-auto ml-auto sm:mr-20 mr-4"
+        @click="openAddCustomer"
       >
-    </p>
-  </footer>
+        <i class="pi pi-plus text-white text-sm m-auto px-0.5"></i>
+        Add Customer
+      </button>
+    </div>
+    <CustomerModal
+      v-if="isModalOpen"
+      :customer="selectedCustomer"
+      @close="closeCustomerModal"
+    />
+    <div class="mt-10">
+      <TanstackTable @edit="openEditCustomer" @delete="deleteCustomer" />
+    </div>
+    <footer class="w-full flex justify-center mt-auto mb-2">
+      <p class="p-4">
+        Design inspo:
+        <a
+          href="https://dribbble.com/shots/2696581-StrefaKoloru-Customers/attachments/544530?mode=media"
+          class="text-sky-950 underline"
+          >Link</a
+        >
+      </p>
+    </footer>
+  </div>
 </template>
